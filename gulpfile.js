31600d203bf019509b7abbaa848955895e9216e1
Nick Wan <nickwanhere@gmail.com>
2015-02-26 18:49:28 +0800

    take out imagemin & js minify when dev, add deploy task instead

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
+var runSequence = require('run-sequence');
 
 // sass
 
@@ -20,6 +21,28 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('build/css'))
         .pipe(reload({stream:true}));
 });
+
+
+gulp.task('sass_dev', function () {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass({errLogToConsole: true})) // コンパイルエラーが起きてもgulpを止めない
+        .pipe(pleeease({
+            autoprefixer: {
+                browsers: ['last 2 versions']
+            }
+        }))
+        .pipe(gulp.dest('build/css'))
+        .pipe(reload({stream:true}));
+});
+
+// js-concat-uglify
+
+gulp.task('js_dev', function() {
+    gulp.src(['js/*.js'])
+        .pipe(gulp.dest('build/js'))
+        .pipe(reload({stream:true}));
+});
+
 
 // js-concat-uglify
 
@@ -55,11 +78,18 @@ gulp.task('bs-reload', function () {
     browserSync.reload();
 });
 
+gulp.task('deploy',function(){
+
+  runSequence('sass',
+              'js', 'imagemin'
+              );
+});
+
+
 // gulp コマンドでなにやるか指定
 
 gulp.task('default',['browser-sync'], function() {
-    gulp.watch('sass/**/*.scss',['sass']);
-    gulp.watch('js/*.js',['js']);
-    gulp.watch('images/**/*.{png,jpg,gif}',['imagemin']);
+    gulp.watch('sass/**/*.scss',['sass_dev']);
+    gulp.watch('js/*.js',['js_dev']);
     gulp.watch("*.html", ['bs-reload']);
 });
